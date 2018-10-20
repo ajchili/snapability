3c4a6fda6e1e122bb9bec68c608db237c6fd42dc
Kirin Patel <kirinpatel@gmail.com>
2018-10-20 17:24:09 -0400

    Installed and setup react-router-dom

--- web/src/App.js
+++ web/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 import "./App.css";
 import Home from "./components/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Post from "./components/Post";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <Header />
-        </header>
-        <Home />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/post" component={Post} />
+        </div>
+      </Router>
     );
   }
 }
