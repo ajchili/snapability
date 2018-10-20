e31fd4c39ae9923b75d2320030422a154bff1492
Kirin Patel <kirinpatel@gmail.com>
2018-10-20 15:57:04 -0400

    Made parse-twitter-post work

--- cloud-functions/parse-twitter-post/main.py
+++ cloud-functions/parse-twitter-post/main.py
@@ -1,12 +1,25 @@
 from bs4 import BeautifulSoup
+import json
 import requests
 
 def parse_twitter_post(request):
-    if request.data and 'post' in [request.data]:
-        post = request.data['post']
+    content_type = request.headers['content-type']
+    if content_type == 'application/json':
+        post = request.json.get('post')
+    elif content_type == 'application/octet-stream':
+        post = request.data
+    elif content_type == 'text/plain':
+        post = request.data
+    elif content_type == 'application/x-www-form-urlencoded':
+        post = request.form.get('post')
+    else:
+        raise ValueError("Unknown content type: {}".format(content_type))
+
+    if post is not None:
         r = requests.get(post)
         soup = BeautifulSoup(r.text, 'html.parser')
         results = soup.find_all('img')
-        return results
+        return str(results)
     else:
         return 'Missing post url!'
+
