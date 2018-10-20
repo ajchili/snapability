577755d4300e3c11231af9c7ea3800442d5b7efd
Kirin Patel <kirinpatel@gmail.com>
2018-10-20 17:55:25 -0400

    Setup basic /post route and started work on fetching twitter post information

--- web/src/App.js
+++ web/src/App.js
@@ -13,7 +13,8 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Home} />
-          <Route path="/post" component={Post} />
+          <Route exact path="/post" component={Post} />
+          <Route path="/post/:type" component={Post} />
         </div>
       </Router>
     );
--- web/src/components/Post.js
+++ web/src/components/Post.js
@@ -1,10 +1,62 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const allowedTypes = [
+  'twitter'
+];
 
 export default class extends Component {
+  state = {
+    post: null,
+  };
+
+  _fetchTweet = url => {
+    axios({
+      method: 'POST',
+      url: 'https://us-central1-snapability-220017.cloudfunctions.net/parse_twitter_post',
+      data: {
+        url
+      }
+    })
+      .then(res => {
+        this.setState({
+          post: res.data
+        });
+      })
+      .catch(err => {
+
+      });
+  }
 
   render() {
+    let type = this.props.match.params.type;
+    let url = null;
+    if (!allowedTypes.includes(type)) type = null;
+    else {
+      switch (type) {
+        case 'twitter':
+          let author = this.props.location.search.split('author=')[1];
+          let status = this.props.location.search.split('status=')[1];
+          if (author && status) {
+            url = `https://twitter.com/${author.split('&')[0]}/status/${status}`;
+            this._fetchTweet(url)
+          }
+          break;
+        default:
+          break;
+      }
+    }
+
     return (
       <div>
+        {type ? (
+          <div>
+            <h3>Type: {type}</h3>
+            {url}
+          </div>
+        ) : (
+          <h3>No type speified.</h3>
+        )}
       </div>
     );
   }
