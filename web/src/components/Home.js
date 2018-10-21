7caea3580a2b3c9f21f6324413e33d6b58d7a453
Kirin Patel <kirinpatel@gmail.com>
2018-10-21 02:29:09 -0400

    Setup UI for Home page

--- web/src/components/Home.js
+++ web/src/components/Home.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
-import Input from "@material-ui/core/Input";
-import TextField from "@material-ui/core/TextField";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
+import {
+  Button,
+  Input,
+  TextField,
+  Grid,
+  MenuItem,
+  FormHelperText,
+  FormControl,
+  Typography,
+  Select
+} from "@material-ui/core";
 
 export default class extends Component {
   state = {
     platform: ""
   };
+
   onPlatformChange = e => {
     this.setState({ platform: e.target.value });
   };
@@ -18,34 +23,68 @@ export default class extends Component {
   render() {
     return (
       <div>
-        <div className="styles.titleDiv">
-          <div>
-            <h1>Snapability</h1>
-            <h4>explain a picture, or whatever.</h4>
-          </div>
-          <div>
-            <TextField
-              id="outlined-dense"
-              label="Enter URL"
-              margin="dense"
-              variant="outlined"
-            />
-          </div>
-          <div>
-            <FormControl>
-              <Select
-                value={this.state.platform}
-                onChange={this.onPlatformChange}
-                input={<Input platform="platform" />}
-              >
-                <MenuItem value={"twitter"}>Twitter</MenuItem>
-                <MenuItem value={"tumblr"}>Tumblr</MenuItem>
-                <MenuItem value={"instagram"}>Instagram</MenuItem>
-              </Select>
-              <FormHelperText>Select platform</FormHelperText>
-            </FormControl>
-          </div>
-        </div>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          style={{
+            height: "100vh"
+          }}
+        >
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            spacing="32"
+          >
+            <Grid item>
+              <Typography variant="display1" align="center">
+                Snapability
+              </Typography>
+              <Typography variant="caption" align="center">
+                an accessible way to experience pictures on social media platforms
+              </Typography>
+            </Grid>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              spacing="32"
+            >
+              <Grid item xs={6} sm={3}>
+                <TextField
+                  id="outlined-dense"
+                  label="Enter URL"
+                  margin="dense"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item>
+                <FormControl>
+                  <Select
+                    value={this.state.platform}
+                    onChange={this.onPlatformChange}
+                    input={<Input platform="platform" />}
+                  >
+                    <MenuItem value={"twitter"}>Twitter</MenuItem>
+                    <MenuItem value={"tumblr"}>Tumblr</MenuItem>
+                    <MenuItem value={"instagram"}>Instagram</MenuItem>
+                  </Select>
+                  <FormHelperText>Select platform</FormHelperText>
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid item>
+            <Button variant="outlined" color="primary">
+              Search
+            </Button>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     );
   }
