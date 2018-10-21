4121311d658defa11ec1c624b3fb48cd1fcf6118
Kirin Patel <kirinpatel@gmail.com>
2018-10-21 01:23:13 -0400

    Fixed POST request

--- cloud-functions/parse-twitter-post/index.js
+++ cloud-functions/parse-twitter-post/index.js
@@ -3,38 +3,24 @@ const cors = require('cors')({ origin: true });
 
 exports.parseTwitterPost = (req, res) => {
   cors(req, res, () => {
-    let url;
-
-    switch (req.get('content-type')) {
-      case 'application/json':
-        url = req.body.url;
-        break;
-      case 'application/octet-stream':
-        url = req.body.toString();
-        break;
-      case 'text/plain':
-        url = req.body;
-        break;
-      case 'application/x-www-form-urlencoded':
-        url = req.body.url;
-        break;
-    }
-
     if (req.method != 'POST') {
       res.status(404).send('Method must be POST!');
-    } else if (!url) {
+    } else if (!req.body.url) {
       res.status(400).send('Missing url!');
     } else {
-      axios.get(url)
+      axios.get(req.body.url)
         .then(result => {
           let data = result.data;
           let tweetURL = data.split('AdaptiveMedia-photoContainer')[1].split('</div>')[0].split('data-image-url="')[1].split('"')[0];
           res.status(200).json({
-            tweet: url,
+            tweet: req.body.url,
             src: tweetURL
           });
         })
-        .catch(err => res.status(500).send(err));
+        .catch(err => {
+          console.error(err);
+          res.status(500).send(err)
+        });
     }
   });
 };
