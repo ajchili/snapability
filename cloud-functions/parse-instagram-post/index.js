934fd48f40c9c6d7d227c81e21d3baa543027978
Elijah <elijahcutler@gmail.com>
2018-10-21 02:59:46 -0400

    Updated functionality for parsing of Instagram posts using javascript

---
+++ cloud-functions/parse-instagram-post/index.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cors = require("cors")({ origin: true });
+
+exports.parseInstagramPost = (req, res) => {
+  cors(req, res, () => {
+    if (req.method != "POST") {
+      res.status(404).send("Method must be POST!");
+    } else if (!req.body.url) {
+      res.status(400).send("Missing url!");
+    } else {
+      axios
+        .get(req.body.url)
+        .then(result => {
+          let data = result.data;
+          let src = data
+            .split('<meta property="og:image" content="')[1]
+            .split('"')[0];
+          //   let src = data
+          //     .split("KL4Bh")[1]
+          //     .split("</div>")[0]
+          //     .split('srcset="')[1]
+          //     .split(" ")[0];
+          res.status(200).json({
+            post: req.body.url,
+            src
+          });
+        })
+        .catch(err => {
+          console.error(err);
+          res.status(500).send(err);
+        });
+    }
+  });
+};
