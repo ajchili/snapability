0803c706e044798698c182f93fbb308b57c029b9
Kirin Patel <kirinpatel@gmail.com>
2018-10-21 05:22:29 -0400

    Added Datastore logging to parseTwitterPost

--- cloud-functions/parse-twitter-post/index.js
+++ cloud-functions/parse-twitter-post/index.js
@@ -1,5 +1,29 @@
 const axios = require('axios');
 const cors = require('cors')({ origin: true });
+const Datastore = require('@google-cloud/datastore');
+
+const projectId = 'snapability-220017';
+
+const saveToDataStore = data => {
+  const datastore = new Datastore({
+    projectId
+  });
+  
+  const kind = 'Parse';
+  const parseKey = datastore.key(kind);
+  
+  const parse = {
+    key: parseKey,
+    type: 'twitter',
+    data,
+    time: new Date().getTime()
+  };
+  
+  datastore
+    .save(parse)
+    .then(() => console.log(`Saved ${parse.key.name}: ${parse.data.description}`))
+    .catch(err => console.error('ERROR:', err));
+};
 
 exports.parseTwitterPost = (req, res) => {
   cors(req, res, () => {
@@ -15,6 +39,12 @@ exports.parseTwitterPost = (req, res) => {
           res.status(200).json({
             tweet: req.body.url,
             src: tweetURL
+          });
+          saveToDataStore({
+            tweet: req.body.url,
+            src: tweetURL,
+            author: req.body.url.split('https://twitter.com/')[1].split('/')[0],
+            post: req.body.url.split('status/')[1],
           });
         })
         .catch(err => {
